259e6e4c731edcc64111a2ea5bd573b370edec7e
Jonathan Copeland <jonathan.copeland@hotmail.co.uk>
2024-02-21 09:08:39 +0000

    Added JavaScript Object, edited html with new tags and improved the css.

--- JavaScript_Drum_Kit/script.js
+++ JavaScript_Drum_Kit/script.js
@@ -1,1 +1,20 @@
+// Creates object to store drum beats.
+
+let drums = {
+    sounds: [
+        "Audio\80s-Bdrum1.wav",
+        "Audio\80s-COWBELL1.wav",
+        "Audio\80s-CRASH2.wav",
+        "Audio\80s-HHCLOSE1.wav",
+        "Audio\80s-HICONGA.wav",
+        "Audio\80s-SNARE1.wav",
+        "Audio\80s-TAMB1.wav",
+        "Audio\80s-TOM1.wav"
+    ],
+
+    function playDrum(){
+       
+    }
+}
+
 
