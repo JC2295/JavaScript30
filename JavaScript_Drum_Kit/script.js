80b4b1473e886cc2bab6be38738ccbd72fb4b72f
Jonathan Copeland <jonathan.copeland@hotmail.co.uk>
2024-02-21 11:32:56 +0000

    Added more JS functionality and fixed file path issue.

--- JavaScript_Drum_Kit/script.js
+++ JavaScript_Drum_Kit/script.js
@@ -1,20 +1,24 @@
 // Creates object to store drum beats.
 
-let drums = {
-    sounds: [
-        "Audio\80s-Bdrum1.wav",
-        "Audio\80s-COWBELL1.wav",
-        "Audio\80s-CRASH2.wav",
-        "Audio\80s-HHCLOSE1.wav",
-        "Audio\80s-HICONGA.wav",
-        "Audio\80s-SNARE1.wav",
-        "Audio\80s-TAMB1.wav",
-        "Audio\80s-TOM1.wav"
-    ],
+let drumSounds = [
+        "Audio/80s-Bdrum1.wav",
+        "Audio/80s-COWBELL1.wav",
+        "Audio/80s-CRASH2.wav",
+        "Audio/80s-HHCLOSE1.wav",
+        "Audio/80s-HICONGA.wav",
+        "Audio/80s-SNARE1.wav",
+        "Audio/80s-TAMB1.wav",
+        "Audio/80s-TOM1.wav"
+    ];
 
-    function playDrum(){
-       
-    }
-}
+let drums = document.querySelectorAll(".drum");
 
+function playDrum(){
+    let audio = new Audio(drumSounds[0]);
 
+    audio.play();
+};
+
+drums[0].addEventListener("click", playDrum);
+
+// js is linked to html
