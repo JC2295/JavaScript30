d16acfa8a151bdc1ac62adae460690cd1fb2d1b9
Jonathan Copeland <jonathan.copeland@hotmail.co.uk>
2024-02-23 14:53:10 +0000

    Added event listeners

--- Image_Filter/script.js
+++ Image_Filter/script.js
@@ -1,29 +1,41 @@
 //Variable declaration and retreival from inputs
 let root = document.documentElement;
 
-function update() {
-
 let colorInput = document.querySelector("#color");
 
-let colorValue = colorInput.getAttribute("value");
+colorInput.addEventListener("change", updateColor);
 
-root.style.setProperty("--color-value", colorValue); 
+function updateColor(){
 
+    let colorValue = colorInput.getAttribute("value");
 
-let blurInput = document.querySelector("#blur");
-
-let blurValue = blurInput.getAttribute("value");
-
-root.style.setProperty("--blur-value", blurValue); 
-
-
-
-let spacingInput = document.querySelector("#spacing");
-
-let spacingValue = spacingInput.getAttribute("value");
-
-root.style.setProperty("--spacing-value", blurValue); 
+    root.style.setProperty("--color-value", colorValue); 
 
 }
 
-setInterval(update, 200);
+let blurInput = document.querySelector("#blur");
+
+blurInput.addEventListener("change", updateBlur);
+
+function updateBlur(){
+
+    let blurValue = blurInput.getAttribute("value");
+
+    root.style.setProperty("--blur-value", blurValue);
+
+}
+
+let spacingInput = document.querySelector("#spacing");
+
+spacingInput.addEventListener("change", updateSpacing);
+
+function updateSpacing(){
+
+    let spacingValue = spacingInput.getAttribute("value");
+
+    root.style.setProperty("--spacing-value", spacingValue); 
+
+}
+
+
+
